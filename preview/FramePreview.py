3ab85e48229814775097c079d16785b0a0f25a72
レオナルド <rynosvapos@gmail.com>
2024-10-02 01:01:45 +0200

    Completed

--- preview/FramePreview.py
+++ preview/FramePreview.py
@@ -1,10 +1,7 @@
-from tkinter import PhotoImage
-from zipfile import sizeEndCentDir
-
 import customtkinter as ctk
-import pywinstyles
 from CTkMenuBar import CTkTitleMenu
 
+# Legenda colori
 appTheme = {
     "button": "red",
     "plot": "green",
@@ -19,42 +16,126 @@ appTheme = {
 class MainApplication(ctk.CTk):
 
     def __main_frame(self):
+        # Sezione dei grafici e dei controlli
         self.main_frame.grid(row=0, column=0, sticky=ctk.NSEW)
+        graph_section_container = ctk.CTkFrame(self.main_frame, fg_color="transparent", corner_radius=0)
+        graph_section_container.pack(side="top", fill="both", expand=True, padx=5, pady=5)
 
+        # Sezione dei grafici
+
+        # Suddivisione finestra
+        plot_container = ctk.CTkFrame(graph_section_container, fg_color="transparent", corner_radius=0)
+        plot_container.pack(side="left", fill="both", expand=True)
+
+        plot_container.columnconfigure(0, weight=1)
+        plot_container.columnconfigure(1, weight=1)
+        plot_container.columnconfigure(2, weight=1)
+
+        plot_container.rowconfigure(0, weight=1)
+        plot_container.rowconfigure(1, weight=1)
+
+        # Grafici
+        bode1_frame = ctk.CTkFrame(plot_container, fg_color=appTheme["plot"], corner_radius=5)
+        bode1_frame.grid(row=0, column=0, padx=(0, 5), pady=(0, 5), sticky=ctk.NSEW)
+
+        bode2_frame = ctk.CTkFrame(plot_container, fg_color=appTheme["plot"], corner_radius=5)
+        bode2_frame.grid(row=1, column=0, padx=(0, 5), sticky=ctk.NSEW)
+
+        nyquist_frame = ctk.CTkFrame(plot_container, fg_color=appTheme["plot"], corner_radius=5)
+        nyquist_frame.grid(row=0, column=1, rowspan=2, columnspan=2, padx=(0, 5), sticky=ctk.NSEW)
+
+        # Sezione dei controlli
+
+        # Suddivisione finestra
+        controller_container = ctk.CTkFrame(graph_section_container, fg_color=appTheme["container"], corner_radius=5)
+        controller_container.pack(side="right", fill="both")
+
+        # Pulsanti
+        tab_container = ctk.CTkFrame(controller_container, fg_color="transparent")
+        tab_container.pack(side="top", fill="both")
+
+        fixed_tab = ctk.CTkButton(tab_container, text="", fg_color=appTheme["button"], height=50, corner_radius=5,
+                                  background_corner_colors=[
+                                      appTheme["mainContainer"], appTheme["button"], appTheme["button"],
+                                      appTheme["button"]
+                                  ])
+        fixed_tab.pack(anchor="n", side="left", fill="x", ipadx=1)
+
+        sweep_tab = ctk.CTkButton(tab_container, text="", fg_color=appTheme["button"], height=50, corner_radius=5,
+                                  background_corner_colors=[
+                                      appTheme["button"], appTheme["mainContainer"], appTheme["button"],
+                                      appTheme["button"]
+                                  ])
+        sweep_tab.pack(anchor="n", side="right", fill="x")
+
+        # Finestre di input
+        input_container = ctk.CTkFrame(controller_container, fg_color="transparent")
+        input_container.pack(side="top", fill="both")
+
+        input1 = ctk.CTkEntry(input_container, fg_color=appTheme["input"], corner_radius=5)
+        input1.pack(side="top", fill="x", padx=5, pady=5)
+
+        input2 = ctk.CTkEntry(input_container, fg_color=appTheme["input"], corner_radius=5)
+        input2.pack(side="top", fill="x", padx=5, pady=5)
+
+        input3 = ctk.CTkEntry(input_container, fg_color=appTheme["input"], corner_radius=5)
+        input3.pack(side="top", fill="x", padx=5, pady=5)
+
+        # Pulsanti di controllo
+        control_container = ctk.CTkFrame(controller_container, fg_color="transparent")
+        control_container.pack(anchor="s", side="bottom", fill="both", pady=(0, 10))
+
+        control_container.columnconfigure(0, weight=1)
+        control_container.columnconfigure(1, weight=1)
+        control_container.columnconfigure(2, weight=1)
+
+        button1 = ctk.CTkButton(control_container, text="", fg_color=appTheme["button"], corner_radius=0, width=70)
+        button1.grid(row=0, column=0)
+
+        button2 = ctk.CTkButton(control_container, text="", fg_color=appTheme["button"], corner_radius=0, width=70)
+        button2.grid(row=0, column=1)
+
+        button3 = ctk.CTkButton(control_container, text="", fg_color=appTheme["button"], corner_radius=0, width=40)
+        button3.grid(row=0, column=2)
+
+        pass
 
     def __bottom_frame(self):
+        # Sezione dati
         self.bottom_frame.grid(row=1, column=0, sticky=ctk.NSEW)
 
-        container = ctk.CTkFrame(self.bottom_frame, fg_color=appTheme["container"], corner_radius=0)
-        container.pack(side="top", fill="both", expand=True)
+        data_container = ctk.CTkFrame(self.bottom_frame, fg_color=appTheme["container"], corner_radius=0)
+        data_container.pack(side="top", fill="both", expand=True, padx=5, pady=(0, 5))
 
-        data_container = ctk.CTkFrame(container, fg_color=appTheme["container"], corner_radius=0)
-        data_container.pack(side="top", fill="both", expand=True, padx = 5, pady = (0,5))
+        # Sezione dei pulsanti
 
+        # Suddivisione finestra
         tool_container = ctk.CTkFrame(data_container, fg_color=appTheme["container"], corner_radius=0)
-        tool_container.pack(side="top", fill="y", anchor="w", padx=30, pady=(5,5))
+        tool_container.pack(side="top", fill="y", anchor="w", padx=30, pady=(5, 5))
 
         tool_container.columnconfigure(0, weight=1)
         tool_container.columnconfigure(1, weight=1)
         tool_container.columnconfigure(2, weight=2)
 
-        button1_frame = ctk.CTkFrame(tool_container, fg_color=appTheme["button"], corner_radius=0, width=40, height=40)
-        button1_frame.grid(row=0, column=0, padx=(0,10))
+        # Pulsanti
+        button1_frame = ctk.CTkButton(tool_container, text="", fg_color=appTheme["button"], corner_radius=0, width=40,
+                                      height=40)
+        button1_frame.grid(row=0, column=0, padx=(0, 10))
 
-        button2_frame = ctk.CTkFrame(tool_container, fg_color=appTheme["button"], corner_radius=0, width=40, height=40)
-        button2_frame.grid(row=0, column=1, padx=(0,30))
+        button2_frame = ctk.CTkButton(tool_container, text="", fg_color=appTheme["button"], corner_radius=0, width=40,
+                                      height=40)
+        button2_frame.grid(row=0, column=1, padx=(0, 30))
 
-        loading_bar_frame = ctk.CTkFrame(tool_container, fg_color=appTheme["loadingBar"], corner_radius=0, width=400, height=20)
+        # Barra di caricamento
+        loading_bar_frame = ctk.CTkFrame(tool_container, fg_color=appTheme["loadingBar"], corner_radius=0, width=400,
+                                         height=20)
+        # loading_bar_frame = ctk.CTkProgressBar(tool_container, fg_color=appTheme["loadingBar"], corner_radius=0, width=400, height=20)
         loading_bar_frame.grid(row=0, column=2)
 
-
-
-        table_frame = ctk.CTkFrame(data_container,fg_color=appTheme["dataContainer"], corner_radius=0)
+        # Tabella dei dati
+        table_frame = ctk.CTkFrame(data_container, fg_color=appTheme["dataContainer"], corner_radius=0)
         table_frame.pack(side="top", fill="both", expand=True)
-
-
-
-
+        pass
 
     def __init__(self, app_theme=None):
         super().__init__()
@@ -74,7 +155,7 @@ class MainApplication(ctk.CTk):
 
         # Suddivisione finestra
         self.main_frame = ctk.CTkFrame(self, fg_color=appTheme["mainContainer"], corner_radius=0)
-        self.bottom_frame = ctk.CTkFrame(self, fg_color=appTheme["mainContainer"], corner_radius=0)
+        self.bottom_frame = ctk.CTkFrame(self, fg_color=appTheme["container"], corner_radius=0)
 
         # Suddivisione griglia
         self.rowconfigure(0, weight=2)
